83dc386cbc60fae21cb3eb43ab57ac175b10ba40
hiarleyCsilva <147431842+hiarleyCsilva@users.noreply.github.com>
2023-10-30 14:20:31 -0300

    Add files via upload

---
+++ atividade30/atividade30.py
@@ -0,0 +1,8 @@
+# Exercício Python 30: Desenvolva um programa que leia o nome, idade e sexo de 4 pessoas.
+# No final do programa, mostre: a média de idade do grupo, qual é o nome do homem mais velho
+# e quantas mulheres têm menos de 20 anos.
+idade= 0
+
+
+for i in range(1, 4):
+    
